9fe7476ab1c7aebccf3663c789ed18e22b47c252
Rico Häuselmann <r.haeuselmann@gmx.ch>
2012-05-01 19:06:07 +0200

    classes für GroupFigure erstellt

---
+++ src/jdraw/ricoh/UngroupAction.java
@@ -0,0 +1,16 @@
+package jdraw.ricoh;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+@SuppressWarnings("serial")
+public class UngroupAction extends AbstractAction {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
