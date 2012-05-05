4d42ac6062c4cb4ca9ace6cc8f041557b2839dcb
Noah S. Bieler <noah.bieler@gmx.ch>
2012-05-05 15:27:48 +0200

    Added Corner Handles

--- src/jdraw/bielern/ASideHandleState.java
+++ src/jdraw/bielern/ASideHandleState.java
@@ -2,6 +2,7 @@ package jdraw.bielern;
 
 import java.awt.Cursor;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 
 import jdraw.framework.DrawView;
@@ -44,6 +45,68 @@ public abstract class ASideHandleState extends ARectHandleState {
 			DrawView v) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static class NState extends ASideHandleState {
+		public NState(int id) {
+			super(id);
+		}
+		@Override
+		public Point getLocation(Figure figure) {
+			Rectangle bounds = figure.getBounds();
+			return new Point(bounds.x + bounds.width / 2, bounds.y);
+		}
+		@Override
+		public Cursor getCursor() {
+			return new Cursor(Cursor.N_RESIZE_CURSOR);
+		}
+	}
+	
+	public static class WState extends ASideHandleState {
+		public WState(int id) {
+			super(id);
+		}
+		@Override
+		public Point getLocation(Figure figure) {
+			Rectangle bounds = figure.getBounds();
+			return new Point(bounds.x, bounds.y + bounds.height / 2);
+		}
+		@Override
+		public Cursor getCursor() {
+			return new Cursor(Cursor.W_RESIZE_CURSOR);
+		}
+	}
+	
+	public static class SState extends ASideHandleState {
+
+		public SState(int id) {
+			super(id);
+		}
+		@Override
+		public Point getLocation(Figure figure) {
+			Rectangle bounds = figure.getBounds();
+			return new Point(bounds.x + bounds.width / 2, (int) bounds.getMaxY());
+		}
+		@Override
+		public Cursor getCursor() {
+			return new Cursor(Cursor.S_RESIZE_CURSOR);
+		}
+	}
+	
+	public static class EState extends ASideHandleState {
+
+		public EState(int id) {
+			super(id);
+		}
+		@Override
+		public Point getLocation(Figure figure) {
+			Rectangle bounds = figure.getBounds();
+			return new Point((int) bounds.getMaxX(), bounds.y + bounds.height / 2);
+		}
+		@Override
+		public Cursor getCursor() {
+			return new Cursor(Cursor.E_RESIZE_CURSOR);
+		}
 	}
 
 }
